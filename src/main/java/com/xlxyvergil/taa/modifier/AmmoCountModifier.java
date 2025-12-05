81254fffcb5d5293f4e7e55b935e49d28ed418f1
xlxyvergil <709228001@qq.com>
2025-12-06 00:58:48 +0800

    1.1.1 弹匣容量兼容GunsmithLib

--- src/main/java/com/xlxyvergil/taa/modifier/AmmoCountModifier.java
+++ src/main/java/com/xlxyvergil/taa/modifier/AmmoCountModifier.java
@@ -40,6 +40,48 @@ public class AmmoCountModifier implements IAttachmentModifier<Modifier, Integer>
     public CacheValue<Integer> initCache(ItemStack gunItem, GunData gunData) {
         // 获取当前的弹匣容量，考虑是否安装了扩容弹匣
         int currentAmmoCount = getCurrentMagazineCapacity(gunItem, gunData);
+        
+        // 尝试使用GunsmithLib的GsHelper来计算AMMO_CAPACITY属性
+        try {
+            // 获取AMMO_CAPACITY属性
+            Class<?> gunAttributesClass = Class.forName("mod.chloeprime.gunsmithlib.api.common.GunAttributes");
+            java.lang.reflect.Field ammoCapacityField = gunAttributesClass.getField("AMMO_CAPACITY");
+            Object ammoCapacityAttributeObj = ammoCapacityField.get(null);
+            
+            // 使用GunsmithLib的GsHelper工具类计算属性值
+            Class<?> gsHelperClass = Class.forName("mod.chloeprime.gunsmithlib.common.util.GsHelper");
+            java.lang.reflect.Method evaluateItemAttributeMethod = gsHelperClass.getMethod(
+                "evaluateItemAttribute", 
+                ItemStack.class, 
+                java.util.function.Supplier.class, 
+                double.class
+            );
+            
+            // 创建Supplier函数接口实例
+            java.util.function.Supplier<Object> attributeSupplier = () -> {
+                try {
+                    Class<?> registryObjectClass = Class.forName("net.minecraftforge.registries.RegistryObject");
+                    java.lang.reflect.Method getMethod = registryObjectClass.getMethod("get");
+                    return getMethod.invoke(ammoCapacityAttributeObj);
+                } catch (Exception e) {
+                    return null;
+                }
+            };
+            
+            // 调用方法计算GunsmithLib修改后的值
+            double gunsmithLibModifiedValue = (Double) evaluateItemAttributeMethod.invoke(
+                null, 
+                gunItem,  // 传入实际的枪械物品
+                attributeSupplier, 
+                (double) currentAmmoCount  // 使用我们的当前值作为基础值
+            );
+            
+            // 使用GunsmithLib的值作为我们的基础值
+            currentAmmoCount = (int) Math.round(gunsmithLibModifiedValue);
+        } catch (Exception e) {
+            // GunsmithLib不存在或调用失败，使用原始值
+        }
+        
         return new CacheValue<>(currentAmmoCount);
     }
 
