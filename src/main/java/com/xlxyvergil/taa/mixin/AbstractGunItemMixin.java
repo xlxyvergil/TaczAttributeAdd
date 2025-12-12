9484eec3366e0a46161b139b56af52388eab8d1c
xlxyvergil <709228001@qq.com>
2025-12-12 22:08:33 +0800

    修复无备弹能换弹问题

--- src/main/java/com/xlxyvergil/taa/mixin/AbstractGunItemMixin.java
+++ src/main/java/com/xlxyvergil/taa/mixin/AbstractGunItemMixin.java
@@ -1,45 +1,38 @@
 package com.xlxyvergil.taa.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.tacz.guns.api.entity.IGunOperator;
 import com.tacz.guns.api.item.gun.AbstractGunItem;
 import com.tacz.guns.resource.modifier.AttachmentCacheProperty;
+import com.tacz.guns.resource.pojo.data.gun.GunData;
+import com.tacz.guns.util.AttachmentDataUtils;
 import com.xlxyvergil.taa.modifier.AmmoCountModifier;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+/**
+ * AbstractGunItem 的 Mixin 类
+ * 设置 ShooterContext 上下文
+ */
 @Mixin(value = AbstractGunItem.class, remap = false)
 public class AbstractGunItemMixin {
     
     /**
-     * 修改 canReload 方法，确保使用修改后的弹匣容量进行装填检查
+     * 在 canReload 方法开始时设置 ShooterContext
      */
-    @ModifyReturnValue(method = "canReload", at = @At("RETURN"), require = 0)
-    private boolean modifyCanReload(boolean original, LivingEntity shooter, ItemStack gun) {
-        // 如果原始返回值为 true，直接返回
-        if (original) {
-            return original;
-        }
-        
-        // 如果原始返回值为 false，检查是否应该基于修改后的容量允许装填
-        if (shooter != null) {
-            IGunOperator operator = IGunOperator.fromLivingEntity(shooter);
-            if (operator != null) {
-                AttachmentCacheProperty cacheProperty = operator.getCacheProperty();
-                if (cacheProperty != null) {
-                    Integer modifiedAmmoCount = cacheProperty.getCache(AmmoCountModifier.ID);
-                    if (modifiedAmmoCount != null && modifiedAmmoCount > 0) {
-                        // 获取当前弹药数
-                        int currentAmmoCount = ((com.tacz.guns.api.item.IGun) (Object) this).getCurrentAmmoCount(gun);
-                        
-                        // 如果当前弹药数小于修改后的弹匣容量，则允许装填
-                        return currentAmmoCount < modifiedAmmoCount;
-                    }
-                }
-            }
-        }
-        return original;
+    @Inject(method = "canReload", at = @At("HEAD"))
+    private void setShooterContext(LivingEntity shooter, ItemStack gunItem, CallbackInfoReturnable<Boolean> ci) {
+        com.xlxyvergil.taa.context.ShooterContext.setShooter(shooter);
+    }
+    
+    /**
+     * 在 canReload 方法结束时清除 ShooterContext
+     */
+    @Inject(method = "canReload", at = @At("RETURN"))
+    private void clearShooterContext(LivingEntity shooter, ItemStack gunItem, CallbackInfoReturnable<Boolean> ci) {
+        com.xlxyvergil.taa.context.ShooterContext.clearShooter();
     }
 }
--- src/main/java/com/xlxyvergil/taa/mixin/AttachmentDataUtilsMixin.java
+++ src/main/java/com/xlxyvergil/taa/mixin/AttachmentDataUtilsMixin.java
@@ -1,51 +1,48 @@
 package com.xlxyvergil.taa.mixin;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.tacz.guns.api.entity.IGunOperator;
 import com.tacz.guns.resource.modifier.AttachmentCacheProperty;
+import com.tacz.guns.resource.pojo.data.gun.FeedType;
 import com.tacz.guns.resource.pojo.data.gun.GunData;
+import com.tacz.guns.resource.pojo.data.gun.GunReloadData;
 import com.tacz.guns.util.AttachmentDataUtils;
-import com.xlxyvergil.taa.context.ShooterContext;
 import com.xlxyvergil.taa.modifier.AmmoCountModifier;
-
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 
+/**
+ * 全局修改 getAmmoCountWithAttachment 方法，确保所有地方都使用 modifier 修改后的弹匣容量
+ */
 @Mixin(value = AttachmentDataUtils.class, remap = false)
 public class AttachmentDataUtilsMixin {
     
-    /**
-     * 修改 getAmmoCountWithAttachment 的返回值
-     * 只处理服务端的情况，客户端由ClientGunPropertyDiagramsMixin处理
-     */
-    @ModifyReturnValue(method = "getAmmoCountWithAttachment", at = @At("RETURN"), require = 0)
-    private static int ammoCapacity(int original, ItemStack gunItem, GunData gunData) {
+    @ModifyReturnValue(method = "getAmmoCountWithAttachment", at = @At("RETURN"))
+    private static int modifyAmmoCountWithAttachment(int original, ItemStack gunItem, GunData gunData) {
         // 检查是否为背包供弹模式，如果是则不修改
-        boolean isUsingInventoryAsMagazine = gunData.getReloadData() != null && 
-            gunData.getReloadData().getType() == com.tacz.guns.resource.pojo.data.gun.FeedType.INVENTORY;
-        if (isUsingInventoryAsMagazine) {
+        GunReloadData reloadData = gunData.getReloadData();
+        if (reloadData != null && reloadData.getType() == FeedType.INVENTORY) {
             return original;
         }
 
-        // 只从ShooterContext获取射手（服务端运行时）
-        LivingEntity shooter = ShooterContext.getShooter();
+        // 使用 ShooterContext 获取射手信息
+        LivingEntity shooter = com.xlxyvergil.taa.context.ShooterContext.getShooter();
         if (shooter != null) {
             IGunOperator operator = IGunOperator.fromLivingEntity(shooter);
             if (operator != null) {
                 AttachmentCacheProperty cacheProperty = operator.getCacheProperty();
                 if (cacheProperty != null) {
                     Integer modifiedAmmoCount = cacheProperty.getCache(AmmoCountModifier.ID);
-                    if (modifiedAmmoCount != null) {
+                    if (modifiedAmmoCount != null && modifiedAmmoCount > 0) {
                         return modifiedAmmoCount;
                     }
                 }
             }
         }
         
-        // 如果没有缓存数据，返回原始值
+        // 如果没有 modifier，返回原始值
         return original;
     }
 }
